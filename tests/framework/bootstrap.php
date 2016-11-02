e2ffbe0a15aa2151ed29b7a8acb6e827def1aa59
Andrew Roslik <andkirby@gmail.com>
2016-11-02 18:36:07 +0200

    Added tests.

---
+++ tests/framework/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+// @codingStandardsIgnoreFile
+/**
+ * @license https://raw.githubusercontent.com/andkirby/commithook/master/LICENSE.md
+ */
+define('PROJECT_ROOT', realpath(__DIR__ . '/../..'));
+
+/** @var Composer\Autoload\ClassLoader $autoloader */
+require realpath(__DIR__ . '/../..') . '/bin/autoload-init.php';
---
+++ tests/phpunit.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sh $(dirname "${BASH_SOURCE[0]}")/../vendor/bin/phpunit $@
